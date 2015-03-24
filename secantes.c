a6647c86667943272875e15a076fc850cdb2aeb3
gilzoide <gilzoide@gmail.com>
2015-03-24 16:48:57 -0300

    Saídas com nomes certos

--- secantes.c
+++ secantes.c
@@ -2,14 +2,14 @@
 
 double secantes (double a, double b, FILE *arquivo) {
 	int i;
-	double x = a, x_anterior = b, erro;
+	double x = a, x_anterior = b, buffer, erro;
 
 	if (f(a) * f(b) > 0) {
 		return FALHOU;
 	}
 
 	for(i = 0; i < MAXITER; i++){
-		double buffer = x;
+		buffer = x;
 
 		x = (f (x) * x_anterior - f (x_anterior) * x) / (f (x) - f (x_anterior));
 		x_anterior = buffer;
@@ -28,15 +28,14 @@ double secantes (double a, double b, FILE *arquivo) {
 
 int main () {
 	// arquivo de saída
-	FILE *arquivo = fopen ("secantes.dat", "w+");
-	if (arquivo == NULL) {
-		fputs ("Erro na abertura do arquivo de saída =S", stderr);
-		return -1;
-	}
-
+	FILE *arquivo = fopen ("secantes_saida1.dat", "w+");
+	assert (arquivo != NULL);
 	printf ("Raiz da f entre [-1, 0]: %lf\n", secantes (-1, 0, arquivo));
-	printf ("Raiz da f entre [0.4, 1]: %lf\n", secantes (0.4, 1, arquivo));
+	fclose (arquivo);
 
+	arquivo = fopen ("secantes_saida2.dat", "w+");
+	assert (arquivo != NULL);
+	printf ("Raiz da f entre [0.4, 1]: %lf\n", secantes (0.4, 1, arquivo));
 	fclose (arquivo);
 
 	return 0;
